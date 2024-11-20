d82f6beb11196b29fd9968f7a4db630abafaae87
Jerry Zhang <jerryzh168@gmail.com>
2024-11-20 10:08:02 -0800

    Extend gpt-fast LLM dashboard to support torchao autoquant
    Summary:
    We want to test autoquant on relevant LLM models
    
    right now only llama2 and mixtral, but want to extend to more models like https://github.com/vllm-project/vllm/tree/main/vllm/model_executor/models
    
    Test Plan:
    https://hud.pytorch.org/benchmark/llms?repoName=pytorch%2Fpytorch
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 5d53d93365f55b035bda9be0840b1943a8b5631d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140627

--- benchmarks/gpt_fast/benchmark.py
+++ benchmarks/gpt_fast/benchmark.py
@@ -5,8 +5,10 @@ import os
 
 from generate import (
     get_arch_name,
+    run_llama2_7b_autoquant,
     run_llama2_7b_bf16,
     run_llama2_7b_int8,
+    run_mixtral_8x7b_autoquant,
     run_mixtral_8x7b_int8,
 )
 
@@ -268,6 +270,8 @@ all_experiments = {
     run_llama2_7b_bf16,
     run_llama2_7b_int8,
     run_mixtral_8x7b_int8,
+    run_mixtral_8x7b_autoquant,
+    run_llama2_7b_autoquant,
     # A list of micro-benchmarks.
     run_mlp_layer_norm_gelu,
     run_layer_norm,
