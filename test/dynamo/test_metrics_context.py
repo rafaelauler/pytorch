77504ec8d28066e0432ce9d72a744ea795e07d2a
Colin L. Rice <clr@fb.com>
2024-11-20 13:42:18 -0700

    pytorch/features: Make a feature logger and record triton bundling
    This modifies metrics_context to allow us to store whether a feature was
    used or not.
    
    This also starts recording this for triton bundling.
    
    ghstack-source-id: 6a7e6ea552945ea24da8c2d2a53c87f863bf92a8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141056

--- test/dynamo/test_metrics_context.py
+++ test/dynamo/test_metrics_context.py
@@ -73,6 +73,15 @@ class TestMetricsContext(TestCase):
 
         self.assertEqual(self.metrics, {"m1": 1, "m2": 2})
 
+    def test_set_key_value(self):
+        with MetricsContext(self._on_exit) as context:
+            context.set_key_value("feature_usage", "k", True)
+            # Overrides allowed
+            context.set_key_value("feature_usage", "k2", True)
+            context.set_key_value("feature_usage", "k2", False)
+
+        self.assertEqual(self.metrics, {"feature_usage": {"k": True, "k2": False}})
+
 
 if __name__ == "__main__":
     run_tests()
